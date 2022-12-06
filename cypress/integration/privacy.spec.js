11ff9a8f069f4b9489e271ec71628852b547af6d
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2022-12-06 00:17:04 -0300

    lidando com links que abrem em outra aba

---
+++ cypress/integration/privacy.spec.js
@@ -0,0 +1,9 @@
+it.only('testa a página da política de privacidade de forma independente', () => {
+    cy.visit('./src/privacy.html');
+
+    cy.title()
+        .should('be.equal', 'Central de Atendimento ao Cliente TAT - Política de privacidade')
+
+    cy.contains('Talking About Testing')
+        .should('be.visible')
+})
