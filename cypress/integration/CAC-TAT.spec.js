f097ae0ba1b813dd646d05a2e279b25982b85441
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2022-12-05 23:51:24 -0300

    test select file

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -184,4 +184,38 @@ describe('Central de Atendimento ao Cliente TAT', () => {
             .uncheck()
             .should('not.be.checked')
     })
+
+    it('seleciona um arquivo da pasta fixtures', () => {
+        cy.get('input[type="file"]')
+            .should('not.have.value')
+            .selectFile('cypress/fixtures/example.json')
+            .should(($input) => {
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+
+            // OUTRA FORMA DE VALIDAR
+            // .then(($input) => {
+            //     const files = $input[0].files
+            //     expect(files[0].name).to.eq('example.json')
+            // })
+    })
+
+    it('seleciona um arquivo simulando um drag-and-drop', () => {
+        cy.get('input[type="file"]')
+            .should('not.have.value')
+            .selectFile('cypress/fixtures/example.json', { action: 'drag-drop' })
+            .should(($input) => {
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+    })
+
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('input[type="file"]')
+            .selectFile('@sampleFile')
+            .should(($input) => {
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+
+    })
 })
